d845d6e9e0f1f15fb79172e4a9038c9fe2c4a9c6
dongyoung <dong5854@gmail.com>
2022-07-20 19:39:35 +0900

    solved.ac class3 1003 파이썬 다시 풀어보기

--- BOJ/solved.ac/class3/python/1003.py
+++ BOJ/solved.ac/class3/python/1003.py
@@ -4,20 +4,43 @@
 
 https://www.acmicpc.net/problem/1003
 """
-# dp를 사용해서 푼다고 한다 해당 내용 공부 후 다시 해보기
-# https://itstory1592.tistory.com/34
+# 다시 직접 해보기
+import sys
 zero = [1, 0, 1]
 one = [0, 1, 1]
 
-def fibonacci(num):
-    length = len(zero)
-    if num >= length:
-        for i in range(length, num+1):
+def solution(n):
+    cur_len = len(zero)
+    if n >= cur_len:
+        for i in range(cur_len, n + 1):
             zero.append(zero[i-1] + zero[i-2])
             one.append(one[i-1] + one[i-2])
-    print('{} {}'.format(zero[num], one[num]))
+    print(zero[n], one[n])
 
-T = int(input())
+N = int(sys.stdin.readline().rstrip())
+
+for _ in range(N):
+    solution(int(sys.stdin.readline().rstrip()))
+
+
+
+
+
+
+# # dp를 사용해서 푼다고 한다 해당 내용 공부 후 다시 해보기
+# # https://itstory1592.tistory.com/34
+# zero = [1, 0, 1]
+# one = [0, 1, 1]
+
+# def fibonacci(num):
+#     length = len(zero)
+#     if num >= length:
+#         for i in range(length, num+1):
+#             zero.append(zero[i-1] + zero[i-2])
+#             one.append(one[i-1] + one[i-2])
+#     print('{} {}'.format(zero[num], one[num]))
+
+# T = int(input())
     
-for _ in range(T):
-    fibonacci(int(input()))
+# for _ in range(T):
+#     fibonacci(int(input()))
