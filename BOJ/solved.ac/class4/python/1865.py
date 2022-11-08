2833de7cb4ebdb989d954c81e019e173df534e52
dong young <dong5854@gmail.com>
2022-11-09 02:03:38 +0900

    solved.ac class4 1865 파이썬

--- BOJ/solved.ac/class4/python/1865.py
+++ BOJ/solved.ac/class4/python/1865.py
@@ -4,8 +4,52 @@
 
 https://www.acmicpc.net/problem/1865
 """
+# 플로이드 워셜 풀이
+import sys
 
-# 플로이드 워샬 오답
+INF = int(1e9)
+
+def floyd(graph, N) -> str:
+    for m in range(1, N+1):
+        for s in range(1, N+1):
+            for e in range(1, N+1):
+                graph[s][e] = min(graph[s][e], graph[s][m] + graph[m][e])
+                if s == e and graph[s][e] < 0:
+                    return "YES"
+    return "NO"
+            
+
+# 테스트케이스의 개수 TC
+TC = int(input())
+
+for _ in range(TC):
+    # 지점의 수 N, 도로의 개수 M, 웜홀의 개수 W
+    N, M, W = map(int, sys.stdin.readline().rstrip().split())
+
+    # 그래프
+    graph = [[INF] * (N+1) for _ in range(N+1)]
+
+    for i in range(1, N+1):
+        graph[i][i] = 0
+
+    # 도로의 정보
+    for _ in range(M):
+        # S와 E는 연결된 지점의 번호, T는 이 도로를 통해 이동하는데 걸리는 시간
+        S, E, T = map(int, sys.stdin.readline().rstrip().split())
+        # 도로는 방향이 없다.
+        graph[S][E] = min(graph[S][E], T)
+        graph[E][S] = min(graph[E][S], T)
+        
+    
+    #웜홀의 정보
+    for _ in range(W):
+        # S와 E는 연결된 지점(시작, 도착)의 번호, T는 줄어드는 시간
+        S, E, T = map(int, sys.stdin.readline().rstrip().split())
+        # 웜홀은 방향이 있다.
+        graph[S][E] = min(graph[S][E], -T)
+    
+    print(floyd(graph, N))
+# 플로이드 워셜 오답
 """
 import sys
 
