10af6d58a32a16646d10a774df28e06cab0c40c5
dong young <dong5854@gmail.com>
2022-11-19 02:42:52 +0900

    solved.ac class4 retry 1074 파이썬

---
+++ BOJ/solved.ac/class3/python/retry/1074.py
@@ -0,0 +1,31 @@
+"""
+1074번
+Z
+
+https://www.acmicpc.net/problem/1074
+"""
+import sys
+
+N, r, c = map(int, sys.stdin.readline().rstrip().split())
+ans = 0
+
+while N > 0:
+    N -= 1
+    # 1번
+    if r < 2 ** N and c < 2 ** N:
+       ans +=  0
+    # 2번
+    elif r < 2 ** N and c >= 2 ** N:
+        ans += (2 ** N) * (2 ** N) * 1
+        c -= (2 ** N)
+    # 3번
+    elif r >= 2 ** N and c < 2 ** N:
+        ans += (2 ** N) * (2 ** N) * 2
+        r -= (2 ** N)
+    # 4번
+    elif r >= 2 ** N and c >= 2 ** N:
+        ans += (2 ** N) * (2 ** N) * 3
+        c -= (2 ** N)
+        r -= (2 ** N)
+
+print(ans)
