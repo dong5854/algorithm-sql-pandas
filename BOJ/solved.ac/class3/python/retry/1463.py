1c38bf50c3f388ecf4ec83480f3347aab226d264
dong young <dong5854@gmail.com>
2022-11-23 20:49:33 +0900

    solved.ac class3 1463 retry 파이썬

---
+++ BOJ/solved.ac/class3/python/retry/1463.py
@@ -0,0 +1,20 @@
+"""
+1463번
+1로 만들기
+
+https://www.acmicpc.net/problem/1463
+"""
+import sys
+
+N = int(sys.stdin.readline().rstrip())
+
+dp = [0] * (N + 1)
+
+for i in range(2, N + 1):
+    dp[i] = dp[i-1]  + 1
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i//3] + 1)
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i//2] + 1)
+
+print(dp[N])
