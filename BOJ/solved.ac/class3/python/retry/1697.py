876630878b6ccd31c3b9c64ae8cf648d5ed4c75d
dong young <dong5854@gmail.com>
2022-11-26 22:43:45 +0900

    solved.ac class3 1697 retry python

--- BOJ/solved.ac/class3/python/retry/1697.py
+++ BOJ/solved.ac/class3/python/retry/1697.py
@@ -4,54 +4,40 @@
 
 https://www.acmicpc.net/problem/1697
 """
-from collections import deque
 import sys
+from collections import deque
 
-def forward(cur):
-    return cur + 1
+def walk_forward(v):
+    return v + 1
 
-def backward(cur):
-    return cur - 1
+def walk_backward(v):
+    return v - 1
 
-def teleport(cur):
-    return cur * 2
+def teleport(v):
+    return 2 * v
+    
+moves = [walk_forward, walk_backward, teleport]
 
-moves = [forward, backward, teleport]
-
-def bfs(graph, N, K):
-    queue = deque()
-    # 큐에 넣기
-    queue.append(N)
-    # 현재 위치는 0
-    graph[N] = 0
-    while queue:
-        # 큐에서 빼기
-        v = queue.popleft()
-        # 현재 움직임 값
-        cur = graph[v]
+def bfs(graph, start, target):
+    graph[start] = 0
+    q = deque()
+    q.append(start)
+    while q:
+        v = q.popleft()
+        cur_time = graph[v]
         for i in moves:
-            # 다음 위치 값
             nv = i(v)
-            # 목표를 잡았을 경우
-            if nv == K:
-                if graph[nv] == -1: # 첫방문
-                    graph[nv] = cur + 1
-                elif graph[nv] > cur + 1:   # 첫방문이 아닌 경우
-                    graph[nv] = cur + 1
+            if nv < 0 or nv > 100_000:
                 continue
-            # 목표물이 없을 겨우, 범위 안에서
-            if nv > 0 and nv <= 100_000:
-                if graph[nv] == -1: # 첫방문
-                    graph[nv] = cur + 1
-                    queue.append(nv)
-                elif graph[nv] > cur + 1:   # 첫방문이 아닌 경우
-                    graph[nv] = cur + 1
-                    queue.append(nv)
-        
+            if graph[nv] == INF:
+                graph[nv] = min(graph[nv], cur_time + 1)
+                if nv != target:
+                    q.append(nv)
 
-N, K = map(int, sys.stdin.readline().rstrip().split())
+N, K = map(int, sys.stdin.readline().split())
 
-graph = [-1 for _ in range(100_001)]
+INF = int(1e9)
+graph = [INF for _ in range(100_001)]
 
 bfs(graph, N, K)
 
