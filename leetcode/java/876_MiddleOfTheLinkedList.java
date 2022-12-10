76d92b3719683acdcfc7bbc518fb101cb87f6020
dong young <dong5854@gmail.com>
2022-12-10 17:20:52 +0900

    leetcode 876. Middle of the Linked List java

--- leetcode/java/876_MiddleOfTheLinkedList.java
+++ leetcode/java/876_MiddleOfTheLinkedList.java
@@ -9,20 +9,37 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// /*
+//  * 처음 한 풀이 시간 복잡도 O(N) + O(N/2)
+//  */
+// class Solution {
+//     public ListNode middleNode(ListNode head) {
+//         int cnt = 0;
+//         ListNode temp = head;
+//         while (temp != null) {
+//             cnt++;
+//             temp = temp.next;
+//         }
+
+//         int mid = cnt / 2 + 1;
+//         ListNode node = head;
+//         for (int i = 1; i < mid; i++) {
+//             node = node.next;
+//         }
+//         return node;
+//     }
+// }
+/*
+ * Fast and Slow Pointer 를 사용한 풀이 시간 복잡도가 O(N)으로 위에보다 효율적이다.
+ */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int cnt = 0;
-        ListNode temp = head;
-        while (temp != null) {
-            cnt++;
-            temp = temp.next;
-        }
-
-        int mid = cnt / 2 + 1;
-        ListNode node = head;
-        for (int i = 1; i < mid; i++) {
-            node = node.next;
-        }
-        return node;
+    ListNode slow = head;
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    return slow;
     }
 }
