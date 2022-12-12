8b68d0ccdb364de6971c98216943c956e62f924f
dong young <dong5854@gmail.com>
2022-12-13 02:09:00 +0900

    leetcode 116. Populating Next Right Pointers in Each Node java

--- leetcode/java/116_PopulatingNextRightPointersInEachNode.java
+++ leetcode/java/116_PopulatingNextRightPointersInEachNode.java
@@ -35,3 +35,22 @@ class Solution {
         setNext(node2.left, node2.right);
     }
 }
+// /*
+//  * 재귀를 이용하지 않은 풀이
+//  */
+// class Solution {
+//     public Node connect(Node root) {
+//         Node level = root;
+//         while(level != null) {
+//             Node cur = level;
+//             while(cur != null) {
+//                 if (cur.left != null) cur.left.next = cur.right;
+//                 if (cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+
+//                 cur = cur.next;
+//             }
+//             level = level.left;
+//         }
+//         return root;
+//     }
+// }
