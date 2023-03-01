b3eb209ee8617f974bf3190222e07137f035ccb5
dong young <dong5854@gmail.com>
2023-03-01 16:08:17 +0900

    BOJ 1052 java

--- BOJ/java/BOJ1052.java
+++ BOJ/java/BOJ1052.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class BOJ1052 {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ1052 {
+
+    static int N;
+    static int K;
+    public static void main(String[] args) throws Exception {
+        getInput();
+        int ans;
+
+        for (ans = 0; ; ans++) {
+            int bottleCnt = 0;
+            int temp = N;
+
+            while (temp != 0) {
+                if (temp % 2 == 1) bottleCnt++;
+                temp /= 2;
+            }
+
+            if (bottleCnt <= K) break;
+            N++;
+        }
+
+        System.out.println(ans);
+    }
+
+    static void getInput() throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        br.close();
+    }
 }
