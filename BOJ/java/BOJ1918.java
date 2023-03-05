4968c9dc254ccc1e194248d91e0da8597946ed9a
dong young <dong5854@gmail.com>
2023-03-06 01:32:55 +0900

    BOJ 1918 java

--- BOJ/java/BOJ1918.java
+++ BOJ/java/BOJ1918.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class BOJ1918 {
+import java.util.*;
+import java.io.*;
+
+public class BOJ1918 {
+
+    static ArrayDeque<Character> stack;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        String formula = br.readLine();
+        stack = new ArrayDeque<>();
+        for (int i = 0;i < formula.length(); i++) {
+            char character = formula.charAt(i);
+            if ('A' <= character && character <= 'Z') { // 알파벳은 그대로 출력
+                sb.append(formula.charAt(i));
+                continue;
+            }
+            if (character == ')') {
+                while(!stack.isEmpty()) {
+                    if (stack.peek() != '(') {
+                        char popped = stack.pop();
+                        sb.append(popped);
+                    } else {
+                        stack.pop();
+                        break;
+                    }
+                }
+                continue;
+            }
+            if (character == '+' || character == '-') {
+                while(!stack.isEmpty()) {
+                    if (stack.peek() != '(') {
+                        char popped = stack.pop();
+                        sb.append(popped);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            if (character == '*' || character == '/') {
+                while(!stack.isEmpty()) {
+                    if (stack.peek() == '*' || stack.peek() == '/') {
+                        char popped = stack.pop();
+                        sb.append(popped);
+                    } else {
+                        break;
+                    }
+                }
+            }
+            stack.push(character);
+        }
+        while(!stack.isEmpty()) {
+            char popped = stack.pop();
+            if (popped == '(') continue;
+            sb.append(popped);
+        }
+
+        System.out.println(sb);
+    }
 }
