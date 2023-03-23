2f332428bf48c780d7f9d66de8fdab2478d985b1
dong young <dong5854@gmail.com>
2023-03-24 00:57:29 +0900

    BOJ 2579 java

--- BOJ/java/BOJ2579.java
+++ BOJ/java/BOJ2579.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class BOJ2579 {
+import java.io.*;
+import java.util.*;
+
+public class BOJ2579 {
+
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N+1];
+        dp = new int[N+1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        if (N == 1) {
+            System.out.println(arr[1]);
+            return;
+        } else if (N == 2) {
+            System.out.println(arr[1] + arr[2]);
+            return;
+        }
+
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]);
+        }
+        System.out.println(dp[N]);
+    }
 }
