caa7a709f651e7a431655459f297826f85649f13
dong5854 <dong5854@gmail.com>
2023-04-10 09:09:09 +0900

    CODETREE hashmap 기본 java

---
+++ codetree/CODETREEhashmap기본.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class CODETREEhashmap기본 {
+
+  static int n;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    n = Integer.parseInt(br.readLine());
+    Map<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < n; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+      String command = st.nextToken();
+      if (command.equals("add")) {
+        int key = Integer.parseInt(st.nextToken());
+        int val = Integer.parseInt(st.nextToken());
+        map.put(key, val);
+      }
+      if (command.equals("find")) {
+        int key = Integer.parseInt(st.nextToken());
+        if (map.containsKey(key)) {
+          System.out.println(map.get(key));
+        } else {
+          System.out.println("None");
+        }
+      }
+      if (command.equals("remove")) {
+        int key = Integer.parseInt(st.nextToken());
+        map.remove(key);
+      }
+    }
+  }
+}
