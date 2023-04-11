8a0a5254eec24ec0170539294b04fc5244a78400
dong5854 <dong5854@gmail.com>
2023-04-11 10:43:46 +0900

    CODETREE 순서를 바꾸었을 때 같은 단어 그룹화하기 java

---
+++ codetree/CODETREE순서를바꾸었을때같은단어그룹화하기.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class CODETREE순서를바꾸었을때같은단어그룹화하기 {
+
+  static Map<String, Integer> group;
+
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    group = new HashMap<>();
+
+    for (int i = 0; i < n; i++) {
+      // 소문자 0~25, 대문자 26~51
+      int[] alpha = new int[52];
+      String word = br.readLine();
+      for (int idx = 0; idx < word.length(); idx++) {
+        char c = word.charAt(idx);
+        if (c >= 'a' && c <= 'z') {
+          alpha[c - 'a'] += 1;
+        } else {
+          alpha[c - 'A' + 26] += 1;
+        }
+      }
+
+      StringBuilder sb = new StringBuilder();
+      for (int idx = 0; idx < alpha.length; idx++) {
+        if (alpha[idx] == 0)
+          continue;
+        // 소문자
+        if (idx >= 0 && idx <= 25) {
+          char c = (char) (idx + 'a');
+          sb.append(c).append(alpha[idx]);
+        } else {
+          char c = (char) (idx - 26 + 'A');
+          sb.append(c).append(alpha[idx]);
+        }
+      }
+
+      group.put(sb.toString(), group.getOrDefault(sb.toString(), 0) + 1);
+    }
+
+    int ans = 0;
+    for (String key : group.keySet()) {
+      if (ans < group.get(key)) {
+        ans = group.get(key);
+      }
+    }
+
+    System.out.println(ans);
+    br.close();
+  }
+}
