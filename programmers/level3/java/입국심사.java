9b520529ed2c06ad1ed75e79ed74eacb62ba8445
이동영 <dong5854@gmail.com>
2023-04-28 23:29:12 +0900

    programmers 입국심사 java

---
+++ programmers/level3/java/입국심사.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+
+    public long solution(int n, int[] times) {
+        long ans = Long.MAX_VALUE;
+        Arrays.sort(times);
+        long left = times[0];
+        long right = (long) times[times.length - 1] * n;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long people = 0;
+            for (int time : times) {
+                people += mid / time;
+            }
+            if (people >= n) {
+                ans = Math.min(ans, mid);
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+}
