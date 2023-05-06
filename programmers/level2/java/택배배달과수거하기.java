ab6fcba5a2474933fd969c9ab602eb7eb1dd4a90
dong young <dong5854@gmail.com>
2023-05-07 02:03:41 +0900

    programmers 택배 배달과 수거하기 java

---
+++ programmers/level2/java/택배배달과수거하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        int deliver = 0; int pickUp = 0;
+        long answer = 0;
+        
+        for (int i = n - 1; i >= 0; i--) {
+            deliver += deliveries[i];
+            pickUp += pickups[i];
+            while (deliver > 0 || pickUp > 0) {
+                deliver -= cap;
+                pickUp -= cap;
+                answer += (i + 1) * 2;
+            }
+        }
+        
+        return answer;
+    }
+}
