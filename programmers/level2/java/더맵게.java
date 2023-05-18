90c3ef8e8a70380333352e14cb9f85133853fbb2
이동영 <dong5854@gmail.com>
2023-05-18 17:55:56 +0900

    programmers 더 맵게 java

---
+++ programmers/level2/java/더맵게.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int s : scoville) {
+            pq.offer(s);
+        }
+        
+        int cnt = 0;
+        
+        while (pq.peek() < K) {
+            cnt++;
+            int first = pq.poll();
+            if (pq.isEmpty()) return -1;
+            int second = pq.poll();
+            pq.offer(first + second * 2);
+        }
+        
+        return cnt;
+    }
+}
