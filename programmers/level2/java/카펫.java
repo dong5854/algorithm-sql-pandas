2cc8017fa9171bfde22573d672e57be62cecab51
dong young <dong5854@gmail.com>
2023-05-20 19:36:19 +0900

    programmers 카펫 java

---
+++ programmers/level2/java/카펫.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int sum = brown + yellow;
+        double sqrt =  Math.floor(Math.sqrt(sum));
+        int[] ans = new int[2];
+        for (int i = (int)sqrt; i <= sum; i++) {
+            if (sum % i == 0) {
+                int width = Math.max(i, sum / i);
+                int height = Math.min(i, sum / i);
+                if ((width-2) * (height-2) == yellow) {
+                    ans[0] = width;
+                    ans[1] = height;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+}
