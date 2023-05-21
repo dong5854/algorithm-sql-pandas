eead0b7d3e9faf5128d73d5eeda7cf9a5adb7228
dong young <dong5854@gmail.com>
2023-05-21 21:41:26 +0900

    programmers H-Index java

---
+++ programmers/level2/java/H-Index.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        Integer[] arr = new Integer[citations.length];
+        for (int i = 0; i < citations.length; i++) {
+            arr[i] = citations[i];
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+        int ans = 0;
+        for (int cnt = 1; cnt <= arr.length; cnt++) {
+            if (arr[cnt-1] >= cnt) {
+                ans = cnt;
+            }
+        }
+        
+        return ans;
+    }
+}
