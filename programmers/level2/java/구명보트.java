c9798c588410497b1ea7582f4373212e7425609f
dong young <dong5854@gmail.com>
2023-05-23 02:27:59 +0900

    programmers 구명보트 java

---
+++ programmers/level2/java/구명보트.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, final int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int sPtr = 0; int bPtr = people.length - 1;
+        int curSum = people[bPtr];
+        while (sPtr <= bPtr) {
+            if (sPtr == bPtr) {
+                answer++;
+                break;
+            }
+            if (curSum + people[sPtr] <= limit) {   // 가벼운 사람을 태울 수 있으면,
+                sPtr++;
+            }
+            answer++;
+            curSum = people[--bPtr];
+        }
+        return answer;
+    }
+}
