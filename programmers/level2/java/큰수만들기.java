4c3b58d66c706ec763987761d9323124c29cc281
dong young <dong5854@gmail.com>
2023-05-25 22:14:21 +0900

    programmers 큰 수 만들기 java

---
+++ programmers/level2/java/큰수만들기.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String number, int k) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+        for (int i = 0; i < number.length(); i++) {
+            while (!stack.isEmpty() && stack.peek() < number.charAt(i) && k > 0) {
+                stack.pop();
+                k--;
+            }
+            stack.push(number.charAt(i));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+        return sb.toString().substring(0, sb.length() - k);
+    }
+}
