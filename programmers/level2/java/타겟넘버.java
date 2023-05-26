245ade6c060d8d2eccb44ed6860307e04b6bbac2
이동영 <dong5854@gmail.com>
2023-05-26 14:04:52 +0900

    programmers 타겟 넘버 java

---
+++ programmers/level2/java/타겟넘버.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    
+    int ans = 0;
+    
+    public int solution(int[] numbers, int target) {
+        
+        dfs(numbers, target, 0, 0);
+        
+        return ans;
+    }
+    
+    public void dfs(int[] numbers, int target, int cnt, int cur) {
+        if (cnt == numbers.length) {
+            if (target == cur) ans++;
+            return;
+        }
+        
+        dfs(numbers, target, cnt + 1, cur + numbers[cnt]);
+        dfs(numbers, target, cnt + 1, cur - numbers[cnt]);
+    }
+}
