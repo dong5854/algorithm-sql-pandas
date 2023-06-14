2181fc95e47e724b911f5dfd8ca4850e11c93ff9
dong young <dong5854@gmail.com>
2023-06-14 19:24:28 +0900

    programmers 오픈채팅방 java

---
+++ programmers/level2/java/오픈채팅방.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<String> solution(String[] record) {
+        List<String> answer = new ArrayList<>();
+        Map<String, String> userInfo = new HashMap<>();
+        
+        for (String r: record) {
+            if (r.contains("Enter") || r.contains("Change")) {
+                String[] info = r.split(" ");
+                userInfo.put(info[1], info[2]);
+            }
+        }
+        
+        for (String r: record) {
+            String[] info = r.split(" ");
+            if (r.contains("Enter")) {
+                answer.add(userInfo.get(info[1]) + "님이 들어왔습니다.");
+            } else if (r.contains("Leave")) {
+                answer.add(userInfo.get(info[1]) + "님이 나갔습니다.");
+            }
+        }
+        
+        return answer;
+    }
+}
