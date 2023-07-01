9df907af2d8c953e853556cf54174009663743c9
dong young <dong5854@gmail.com>
2023-07-02 00:15:13 +0900

    leetcode container with most water java

--- leetcode/java/11_ContainerWithMostWater.java
+++ leetcode/java/11_ContainerWithMostWater.java
@@ -19,3 +19,27 @@ class Solution {
         return ans;
     }
 }
+
+/**
+ * 더 빠른 풀이
+ */
+// class Solution {
+//     public int maxArea(int[] height) {
+//         int left = 0;
+//         int right = height.length - 1;
+//         int maxArea = 0;
+
+//         while (left < right) {
+//             int currentArea = Math.min(height[left], height[right]) * (right - left);
+//             maxArea = Math.max(maxArea, currentArea);
+
+//             if (height[left] < height[right]) {
+//                 left++;
+//             } else {
+//                 right--;
+//             }
+//         }
+
+//         return maxArea;
+//     }
+// }
