797867553c32767eeba8a39a1e5ed1a7e1e8d026
dong young <dong5854@gmail.com>
2023-07-22 22:50:22 +0900

    leetcode 206. Reverse Linked List java

--- leetcode/java/206_ReverseLinkedList.java
+++ leetcode/java/206_ReverseLinkedList.java
@@ -24,3 +24,28 @@ class Solution {
         return head;
     }
 }
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//         if(head == null || head.next == null) return head;
+//         ListNode ans = null;
+//         while(head != null) {
+//             ListNode node = head;
+//             head = head.next;
+//             node.next = ans;
+//             ans = node;
+//         }
+
+//         return ans;
+//     }
+// }
