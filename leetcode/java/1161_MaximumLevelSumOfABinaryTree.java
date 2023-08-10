1ed94d462832f9c522f2bda020e20399aea13e1a
dong young <dong5854@gmail.com>
2023-08-10 19:47:13 +0900

    leetcode 1161. Maximum Level Sum of a Binary Tree java

--- leetcode/java/1161_MaximumLevelSumOfABinaryTree.java
+++ leetcode/java/1161_MaximumLevelSumOfABinaryTree.java
@@ -10,3 +10,25 @@
         dfs(n.left, l, level + 1);
         dfs(n.right, l, level + 1);
     }
+
+    class Solution {
+    public int maxLevelSum(TreeNode root) {
+        
+        List<Integer> levels = new ArrayList<>();
+        getAns(root, 0, levels);
+
+        return levels.indexOf(Collections.max(levels)) + 1;
+    }
+
+    private void getAns(TreeNode node, int level, List<Integer> levels) {
+        if (node == null) return;
+        if (levels.size() <= level) {
+            levels.add(node.val);
+        } else {
+            levels.set(level, levels.get(level) + node.val);
+        }
+
+        getAns(node.left, level + 1, levels);
+        getAns(node.right, level + 1, levels);
+    }
+}
