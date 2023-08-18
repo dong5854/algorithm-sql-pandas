8e1a02ee6094d1ce042589e77949e055f1fa8bb2
dong young <dong5854@gmail.com>
2023-08-18 22:03:43 +0900

    solved.ac 11654 java

---
+++ BOJ/solved.ac/새싹/문자열/java/BOJ11654.java
@@ -0,0 +1,34 @@
+/*
+11654번
+아스키 코드
+
+알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+https://www.acmicpc.net/problem/11654
+ */
+import java.util.*;
+
+class BOJ11654 {
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    String input = sc.next();
+
+    char character = input.charAt(0);
+
+    System.out.println((int) character);
+
+    sc.close();
+  }
+}
+/*
+System.in.read()를 사용하면 더 짧게 가능합니다.
+
+import java.io.IOException;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		int ch = System.in.read();
+		System.out.println(ch);
+	}
+}
+*/
