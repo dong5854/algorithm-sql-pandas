0ce04e2e623f4c65fb9c58d358ed081e932039e6
dong young <dong5854@gmail.com>
2023-09-03 14:17:17 +0900

    codetree 두 수의 합 java

--- codetree/java/CODETREE두수의합.java
+++ codetree/java/CODETREE두수의합.java
@@ -34,3 +34,34 @@ public class CODETREE두수의합 {
     System.out.println(ans);
   }
 }
+/**
+ * 2021-09-03 다시 풀이
+ */
+// import java.util.*;
+// import java.io.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+//         int n = Integer.parseInt(st.nextToken());
+//         int k = Integer.parseInt(st.nextToken());
+
+//         st = new StringTokenizer(br.readLine());
+//         HashMap<Integer, Integer> map = new HashMap<>();
+//         List<Integer> list = new ArrayList<>();
+//         for (int i = 0; i < n; i++) {
+//             int num = Integer.parseInt(st.nextToken());
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//             list.add(num);
+//         }
+        
+//         int ans = 0;
+//         for (int num : list) {
+//             if (map.containsKey(num)) map.put(num, map.get(num) - 1);
+//             ans += map.getOrDefault(k - num, 0);
+//         }
+//         System.out.println(ans);
+//         br.close();
+//     }
+// }
