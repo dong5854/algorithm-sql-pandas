9dedf80a609e0fe946615f7cee405607914062cb
dong young <dong5854@gmail.com>
2023-09-03 15:23:11 +0900

    codetree 세 수의 합 java

--- codetree/java/CODETREE세수의합.java
+++ codetree/java/CODETREE세수의합.java
@@ -40,3 +40,41 @@ public class CODETREE세수의합 {
     System.out.println(ans);
   }
 }
+/**
+ * 2021-09-03 다시 풀이
+ */
+// import java.util.*;
+// import java.io.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine());
+
+//         List<Integer> list = new ArrayList<>();
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         int n = Integer.parseInt(st.nextToken());
+//         int k = Integer.parseInt(st.nextToken());
+
+//         st = new StringTokenizer(br.readLine());
+//         for (int i = 0; i < n; i++) {
+//             int num = Integer.parseInt(st.nextToken());
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//             list.add(num);
+//         }
+
+//         int ans = 0;
+//         for(int i = 0; i < n; i++) { // 첫 번째
+//             int num1 = list.get(i);
+//             map.put(num1, map.get(num1) - 1);
+//             for (int j = 0; j < i; j++) { // 두 번째
+//                 int num2 = list.get(j);
+//                 if (map.containsKey(k - num1 - num2)) ans += map.get(k - num1 - num2);
+//             }
+//         }
+
+//         System.out.println(ans);
+//         br.close();
+//     }
+// }
