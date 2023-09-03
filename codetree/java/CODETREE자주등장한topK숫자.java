59650bb454d655a08e9fb8f36b08228fa5bc97c2
dong young <dong5854@gmail.com>
2023-09-04 00:04:47 +0900

    codetree 자주 등장한 top K 숫자 java

--- codetree/java/CODETREE자주등장한topK숫자.java
+++ codetree/java/CODETREE자주등장한topK숫자.java
@@ -31,3 +31,58 @@ public class CODETREE자주등장한topK숫자 {
     }
   }
 }
+/**
+ * 2021-09-04 다시풀기
+ */
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+
+//     static class Pair implements Comparable<Pair> {
+
+//         public int num;
+//         public int cnt;
+
+//         public Pair(int num, int cnt) {
+//             this.num = num;
+//             this.cnt = cnt;
+//         }
+
+//         @Override
+//         public int compareTo(Pair p) {
+//             int c = -Integer.compare(this.cnt, p.cnt);
+//             if (c == 0) c = -Integer.compare(this.num, p.num);
+//             return c;
+//         }
+        
+//     }
+
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//         int n = Integer.parseInt(st.nextToken());
+//         int k = Integer.parseInt(st.nextToken());
+
+//         Map<Integer, Integer> map = new HashMap<>();
+//         st = new StringTokenizer(br.readLine(), " ");
+//         for (int i = 0; i < n; i++) {
+//             int num = Integer.parseInt(st.nextToken());
+//             map.put(num, map.getOrDefault(num, 0) + 1);
+//         }
+
+//         List<Pair> list = new ArrayList<>();
+//         for (int key : map.keySet()) {
+//             list.add(new Pair(key, map.get(key)));
+//         }
+//         Collections.sort(list);
+
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < k; i++) {
+//             sb.append(list.get(i).num).append(" ");
+//         }
+
+//         System.out.println(sb);
+//         br.close();
+//     }
+// }
