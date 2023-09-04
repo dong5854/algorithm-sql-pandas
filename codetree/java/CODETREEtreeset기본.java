c32a7405976036dd650e24fe11aeadaed65a1e12
dong young <dong5854@gmail.com>
2023-09-04 23:05:00 +0900

    CODETREE treeset 기본 java

--- codetree/java/CODETREEtreeset기본.java
+++ codetree/java/CODETREEtreeset기본.java
@@ -68,3 +68,48 @@ public class CODETREEtreeset기본 {
     }
   }
 }
+/**
+ * 2021-09-04
+ */
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         Map<Integer, Integer> map = new TreeMap<>();
+//         int n = Integer.parseInt(br.readLine());
+        
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < n;i++) {
+//             String[] command = br.readLine().split(" ");
+//             switch(command[0]) {
+//                 case "add":
+//                     map.put(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+//                     break;
+//                 case "find":
+//                 if (map.containsKey(Integer.parseInt(command[1])))
+//                     sb.append(map.get(Integer.parseInt(command[1]))).append("\n");
+//                 else
+//                     sb.append("None").append("\n");
+//                 break;
+//                 case "remove":
+//                     map.remove(Integer.parseInt(command[1]));
+//                     break;
+//                 case "print_list":
+//                     if (map.isEmpty()) {
+//                         sb.append("None\n");
+//                         break;
+//                     }
+//                     for (Integer key : map.keySet()) {
+//                         sb.append(map.get(key)).append(" ");
+//                     }
+//                     sb.append("\n");
+//                     break;
+//             }
+//         }
+
+//         System.out.println(sb);
+//         br.close();
+//     }
+// }
