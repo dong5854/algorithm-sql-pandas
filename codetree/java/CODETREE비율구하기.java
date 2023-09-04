5d0d828ee9b95fc3fd8b99050c78eeb01313b602
dong young <dong5854@gmail.com>
2023-09-04 23:06:46 +0900

    codetree 비율 구하기 java

--- codetree/java/CODETREE비율구하기.java
+++ codetree/java/CODETREE비율구하기.java
@@ -22,3 +22,25 @@ public class CODETREE비율구하기 {
     br.close();
   }
 }
+/**
+ * 2023-09-04
+ */
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int n = Integer.parseInt(br.readLine());
+//         Map<String, Integer> map = new TreeMap<>();
+//         for (int i = 0; i < n; i++) {
+//             String word = br.readLine();
+//             map.put(word, map.getOrDefault(word, 0) + 1);
+//         }
+
+//         for (String word : map.keySet()) {
+//             System.out.printf("%s %.4f\n", word, (map.get(word) / (double) n) * 100);
+//         }
+//         br.close();
+//     }
+// }
