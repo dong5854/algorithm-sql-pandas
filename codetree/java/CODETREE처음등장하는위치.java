558267bbc08e31a5b208917cef51e4e453934092
dong young <dong5854@gmail.com>
2023-09-05 23:34:56 +0900

    codetree 처음 등장하는 위치 java

--- codetree/java/CODETREE처음등장하는위치.java
+++ codetree/java/CODETREE처음등장하는위치.java
@@ -22,3 +22,34 @@ public class CODETREE처음등장하는위치 {
     br.close();
   }
 }
+/**
+ * 2023-09-05
+ */
+// import java.util.stream.*;
+// import java.util.*;
+// import java.io.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int n = Integer.parseInt(br.readLine());
+//         Map<Integer, Integer> map = new TreeMap<>();
+
+//         int[] arr = Arrays.stream(br.readLine().split(" "))
+//                     .mapToInt(Integer::parseInt)
+//                     .toArray(); 
+
+//         IntStream
+//             .range(0, arr.length)
+//             .forEach(i -> map.putIfAbsent(arr[i], i+1));
+
+//         StringBuilder sb = new StringBuilder();
+//         for (int key : map.keySet()) {
+//             sb.append(key).append(" ").append(map.get(key)).append("\n");
+//         }
+
+//         System.out.println(sb);
+
+//         br.close();
+//     }
+// }
