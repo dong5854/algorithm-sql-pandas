682ece9d3cce24b1d9395aa6a107d0fcd8bb6eaa
dong young <dong5854@gmail.com>
2023-09-07 00:37:18 +0900

    codetree 대칭차집합 java

--- codetree/java/CODETREE대칭차집합.java
+++ codetree/java/CODETREE대칭차집합.java
@@ -38,3 +38,43 @@ public class CODETREE대칭차집합 {
     br.close();
   }
 }
+/**
+ * 2023-09-07
+ */
+// import java.io.*;
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         br.readLine();
+//         Set<Integer> A = Arrays.stream(br.readLine().split(" ")).
+//         map(Integer::parseInt).
+//         collect(Collectors.toCollection(HashSet::new));
+
+//         Set<Integer> B = Arrays.stream(br.readLine().split(" ")).
+//         map(Integer::parseInt).
+//         collect(Collectors.toCollection(HashSet::new));
+
+//         Set<Integer> ans = new HashSet<>();
+//         Set<Integer> toRemove = new HashSet<>();
+//         for (int a : A) {
+//             if (B.contains(a)) toRemove.add(a);
+//             ans.add(a);
+//         }
+
+//         for (int b : B) {
+//             if (A.contains(b)) toRemove.add(b);
+//             ans.add(b);
+//         }
+
+//         for (int r : toRemove) {
+//             ans.remove(r);
+//         }
+
+//         System.out.println(ans.size());
+
+//         br.close();
+//     }
+// }
