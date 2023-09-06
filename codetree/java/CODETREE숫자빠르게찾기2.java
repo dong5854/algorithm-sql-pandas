b504e8f538eb547e83ac81423dd7684053d3d6d7
dong young <dong5854@gmail.com>
2023-09-07 00:39:25 +0900

    codetree 숫자 빠르게 찾기 2 java

--- codetree/java/CODETREE숫자빠르게찾기2.java
+++ codetree/java/CODETREE숫자빠르게찾기2.java
@@ -30,3 +30,33 @@ public class CODETREE숫자빠르게찾기2 {
     br.close();
   }
 }
+/**
+ * 2023-09-07
+ */
+// import java.io.*;
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//         int n = Integer.parseInt(st.nextToken());
+//         int m = Integer.parseInt(st.nextToken());
+
+//         TreeSet<Integer> set = Arrays.stream(br.readLine().split(" ")).
+//         map(Integer::parseInt).
+//         collect(Collectors.toCollection(TreeSet::new));
+
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 0; i < m; i++) {
+//             int num = Integer.parseInt(br.readLine());
+//             if (set.ceiling(num) == null) sb.append(-1).append("\n");
+//             else sb.append(set.ceiling(num)).append("\n");
+//         }
+
+//         System.out.println(sb);
+
+//         br.close();
+//     }
+// }
