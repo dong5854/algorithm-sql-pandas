3557fcf6007e6fa4308633a41b1bab5fa7201586
dong young <dong5854@gmail.com>
2023-09-07 00:39:44 +0900

    codetree 정수명령어 java

--- codetree/java/CODETREE정수명령어.java
+++ codetree/java/CODETREE정수명령어.java
@@ -34,3 +34,48 @@ public class CODETREE정수명령어 {
     br.close();
   }
 }
+/**
+ * 2023-09-07
+ */
+// import java.io.*;
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Main {
+
+//     static int T, k;
+
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         T = Integer.parseInt(br.readLine());
+//         StringBuilder sb = new StringBuilder();
+//         for (int tc = 0; tc < T; tc++) {
+//             k = Integer.parseInt(br.readLine());
+//             TreeSet<Integer> set = new TreeSet<>();
+//             for (int i = 0; i < k; i++) {
+//                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//                 String command = st.nextToken();
+//                 int num = Integer.parseInt(st.nextToken());
+//                 switch(command) {
+//                     case "I":
+//                         set.add(num);
+//                         break;
+//                     case "D":
+//                         if (set.isEmpty()) break;
+//                         else if (num == -1) set.remove(set.first());
+//                         else set.remove(set.last());
+//                         break;
+//                     default:
+//                         break;
+//                 }
+//             }
+//             if (set.isEmpty()) sb.append("EMPTY");
+//             else sb.append(set.last()).append(" ").append(set.first());
+//             sb.append("\n");
+//         }
+
+//         System.out.println(sb);
+
+//         br.close();
+//     }
+// }
