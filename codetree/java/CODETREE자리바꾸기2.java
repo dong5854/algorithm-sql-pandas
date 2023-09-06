1e2a1888b0c128a1a6698dd64f4ce834fdf5075a
dong young <dong5854@gmail.com>
2023-09-07 00:40:18 +0900

    codetree 자리바꾸기 2 java

--- codetree/java/CODETREE자리바꾸기2.java
+++ codetree/java/CODETREE자리바꾸기2.java
@@ -50,3 +50,54 @@ public class CODETREE자리바꾸기2 {
     br.close();
   }
 }
+/**
+ * 2023-09-05
+ */
+// import java.io.*;
+// import java.util.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+//         int N = Integer.parseInt(st.nextToken());
+//         int K = Integer.parseInt(st.nextToken());
+
+//         int[] seats = new int[N+1];
+//         Map<Integer, Set<Integer>> ans = new HashMap<>();
+//         for (int i = 1; i <= N; i++) {
+//             seats[i] = i;
+//             ans.put(i, new HashSet<>());
+//             ans.get(i).add(i);
+//         }
+
+//         List<int[]> commands = new ArrayList<>();
+//         for (int i = 0; i < K; i++) {
+//             st = new StringTokenizer(br.readLine(), " ");
+//             int a = Integer.parseInt(st.nextToken());
+//             int b = Integer.parseInt(st.nextToken());
+//             commands.add(new int[]{a, b});
+//         }
+
+//         for (int i = 0; i < 3; i++) {
+//             for (int[] c: commands) {
+//                 int a = c[0];
+//                 int b = c[1];
+//                 ans.get(seats[a]).add(b);
+//                 ans.get(seats[b]).add(a);
+//                 int temp = seats[b];
+//                 seats[b] = seats[a];
+//                 seats[a] = temp;
+//             }
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         for (int i = 1; i <= N; i++) {
+//             sb.append(ans.get(i).size()).append("\n");
+//         }
+
+//         System.out.println(sb);
+//         br.close();
+//     }
+// }
