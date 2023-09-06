ee39818cecb821eb1bfd1c1639b2be96c49299d7
dong young <dong5854@gmail.com>
2023-09-07 00:37:53 +0900

    codetree 데이터 비교 java

--- codetree/java/CODETREE데이터비교.java
+++ codetree/java/CODETREE데이터비교.java
@@ -24,3 +24,32 @@ public class CODETREE데이터비교 {
     br.close();
   }
 }
+/**
+ * 2023-09-05
+ */
+// import java.io.*;
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Main {
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         int n = Integer.parseInt(br.readLine());
+//         Set<Integer> set = Arrays.
+//         stream(br.readLine().split(" ")).
+//         map(Integer::parseInt).
+//         collect(Collectors.toCollection(HashSet::new));
+        
+//         int m = Integer.parseInt(br.readLine());
+//         StringBuilder sb = new StringBuilder();
+//         Arrays.stream(br.readLine().split(" ")).
+//             map(Integer::parseInt).
+//             forEach(x -> {
+//                 if (set.contains(x)) sb.append(1).append(" ");
+//                 else sb.append(0).append(" ");
+//             });
+
+//         System.out.println(sb);
+//         br.close();
+//     }
+// }
