67c221c577afbb4d462a25e03a2b8c72303369a8
dong young <dong5854@gmail.com>
2023-09-08 00:43:28 +0900

    codetree 최대숫자 구하기 java

--- codetree/java/CODETREE최대숫자구하기.java
+++ codetree/java/CODETREE최대숫자구하기.java
@@ -25,3 +25,36 @@ public class CODETREE최대숫자구하기 {
     br.close();
   }
 }
+/**
+ * 2923-09-08
+ */
+// import java.io.*;
+// import java.util.*;
+// import java.util.stream.*;
+
+// public class Main {
+
+//     static int n,m;
+
+//     public static void main(String[] args) throws Exception {
+//         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//         n = Integer.parseInt(st.nextToken());
+//         m = Integer.parseInt(st.nextToken());
+        
+//         TreeSet<Integer> set = IntStream.rangeClosed(1, m).
+//         boxed().
+//         collect(Collectors.toCollection(TreeSet::new));
+
+//         StringBuilder sb = new StringBuilder();
+//         Arrays.stream(br.readLine().split(" ")).
+//         map(Integer::parseInt).
+//         forEach((num) -> {
+//             set.remove(num);
+//             sb.append(set.last()).append("\n");
+//         });
+
+//         System.out.println(sb);
+//         br.close();
+//     }
+// }
