9ec4bff67219d6f77d719213aaccc378ca5f1f44
dong young <dong5854@gmail.com>
2023-09-10 20:24:08 +0900

    codetree 배열 추출 java

---
+++ codetree/java/CODETREE배열추출.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class CODETREE배열추출 {
+
+    static int n;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                Integer polled = pq.poll();
+                if (polled == null) polled = 0;
+                sb.append(polled).append("\n");
+            }
+            else pq.add(num);
+        }
+
+        System.out.println(sb);
+
+        br.close();
+    }
+}
