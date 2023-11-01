55fd60a32eb09fb60e997bd1558aac5569a3cb94
dong young <dong5854@gmail.com>
2023-11-01 22:28:57 +0900

    codetree 연속 부분합의 최댓값 구하기 2 java

--- codetree/java/CODETREE_연속부분합의최댓값구하기2.java
+++ codetree/java/CODETREE_연속부분합의최댓값구하기2.java
@@ -1,35 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
-public class CODETREE_연속부분합의최댓값구하기2 {    
-    public static int INT_MIN = Integer.MIN_VALUE;
-    public static final int MAX_N = 100000;
-    
-    public static int n;
-    public static int[] a = new int[MAX_N + 1];
+public class CODETREE_연속부분합의최댓값구하기2 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        \
-        n = sc.nextInt();
-        for(int i = 1; i <= n; i++)
-            a[i] = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
 
-        int ans = INT_MIN;
-
-
+        int ans = Integer.MIN_VALUE;
         int sum = 0;
 
-        for(int i = 1; i <= n; i++) {
-
-            if(sum < 0)
-                sum = a[i];
-            
-            else
-                sum += a[i];
-            
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(st.nextToken());
             ans = Math.max(ans, sum);
+            if (sum < 0) sum = 0;
         }
-        
-        System.out.print(ans);
+
+        System.out.println(ans);
+
+        br.close();
     }
 }
