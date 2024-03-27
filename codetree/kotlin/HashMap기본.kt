327b03ef613762b958efdb9f7f629264fae1b7d5
dong young <dong5854@gmail.com>
2024-03-28 00:18:20 +0900

    codetree HashMap 기본 kotlin

--- codetree/kotlin/HashMap기본.kt
+++ codetree/kotlin/HashMap기본.kt
@@ -1,21 +1,20 @@
 import java.io.*;
 
-val br = BufferedReader(InputStreamReader(System.`in`))
-val N = br.readLine().toInt()
-val map = HashMap<Int, Int>();
-
 fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    val N = br.readLine().toInt()
+    val map = HashMap<Int, Int>();
 
     for (i in 1..N) {
         val input = br.readLine().split(" ")
         if (input.size == 2)
-            printResult(input[0], input[1].toInt())
+            printResult(map, input[0], input[1].toInt())
         else
-            printResult(input[0], input[1].toInt(), input[2].toInt())
+            printResult(map, input[0], input[1].toInt(), input[2].toInt())
     }
 }
 
-fun printResult(command: String, input1: Int, input2: Int = 0) {
+fun printResult(map: HashMap<Int, Int>, command: String, input1: Int, input2: Int = 0) {
     if (command == "add")
        map[input1] = input2
     else if (command == "find")
