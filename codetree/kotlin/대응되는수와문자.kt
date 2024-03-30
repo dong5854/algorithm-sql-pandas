e52906e34997bc6f13482f7b782d82f88cb4302e
dong young <dong5854@gmail.com>
2024-03-30 22:47:16 +0900

    codetree 대응되는 수와 문자 kotlin

---
+++ codetree/kotlin/대응되는수와문자.kt
@@ -0,0 +1,9 @@
+import java.io.*
+import java.util.*
+
+fun main() {
+    val br = BufferedReader(InputStreamReader(System.`in`))
+    var st = StringTokenizer(br.readLine(), " ")
+    val n = st.nextToken().toInt()
+    val m = st.nextToken().toInt()
+}
