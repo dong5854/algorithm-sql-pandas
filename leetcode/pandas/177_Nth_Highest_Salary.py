bff7635d8f8be018ed94af2659849d562ae75752
이동영 <dong5854@gmail.com>
2024-06-02 22:37:18 +0900

    leetcode 177_Nth_Highest_Salary pandas

--- leetcode/pandas/177_Nth_Highest_Salary.py
+++ leetcode/pandas/177_Nth_Highest_Salary.py
@@ -1,13 +1,11 @@
 import pandas as pd
 
 def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    unique_salaries = employee['salary'].drop_duplicates().sort_values(ascending=False)
 
-    drop_duplicates = employee['salary'].drop_duplicates()
-    employee_sorted = drop_duplicates.sort_values()
-
-    if N > len(employee):
+    if N > len(unique_salaries) or N <= 0:
         return pd.DataFrame({f"getNthHighestSalary({N})": [None]})
 
-    nth_highest = employee_sorted.iloc[N - 1]
+    nth_highest = unique_salaries.iloc[N - 1]
 
     return pd.DataFrame({f"getNthHighestSalary({N})": [nth_highest]})
