815affd7a0ec21a87ba6d14e6f2ecdb053d030ce
dong young <dong5854@gmail.com>
2024-09-27 03:42:28 +0900

    leetcode: 180. Consecutive Numbers java

---
+++ leetcode/postgreSQL/180_Consecutive_Numbers.sql
@@ -0,0 +1,13 @@
+WITH NumberedLogs AS (
+    SELECT 
+        id,
+        num,
+        LAG(num, 1) OVER (ORDER BY id) AS prev_num,
+        LAG(num, 2) OVER (ORDER BY id) AS prev_num2
+    FROM 
+        Logs
+)
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM NumberedLogs
+WHERE num = prev_num AND num = prev_num2;
