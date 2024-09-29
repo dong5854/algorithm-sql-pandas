d32abde78777d13822242628af032ce2ce17e1f1
dong young <dong5854@gmail.com>
2024-09-30 01:09:58 +0900

    programmers: 택배 배달과 수거하기 Java

--- programmers/level2/java/택배배달과수거하기.java
+++ programmers/level2/java/택배배달과수거하기.java
@@ -16,3 +16,61 @@ class 택배배달과수거하기 {
         return answer;
     }
 }
+
+// import java.util.*;
+
+// class 택배배달과수거하기 {
+//     public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+                    
+            
+//         // {거리, 필요한 수}
+//         ArrayDeque<int[]> dStack = new ArrayDeque<>();
+//         ArrayDeque<int[]> pStack = new ArrayDeque<>();
+        
+//         for (int i = 0; i < n; i++) {
+//             if (deliveries[i] > 0) dStack.push(new int[]{i + 1, deliveries[i]});
+//             if (pickups[i] > 0) pStack.push(new int[]{i + 1, pickups[i]});
+//         }
+        
+//         long answer = 0;
+//         while (!dStack.isEmpty() || !pStack.isEmpty()) {
+            
+//             int pickupCap = cap;
+//             int deliverCap = cap;
+            
+//             if (!dStack.isEmpty() && !pStack.isEmpty()) {
+//                 answer += Math.max(dStack.peek()[0], pStack.peek()[0]) * 2;
+//             } else if(!dStack.isEmpty()) {
+//                 pickupCap = 0;
+//                 answer += dStack.peek()[0] * 2;
+//             } else if(!pStack.isEmpty()) {
+//                 deliverCap = 0;
+//                 answer += pStack.peek()[0] * 2;
+//             }
+            
+//             while (pickupCap != 0 && !pStack.isEmpty()) {
+//                 int[] temp = pStack.pop();
+//                 if (temp[1] <= pickupCap) {
+//                     pickupCap -= temp[1];
+//                 } else {
+//                     temp[1] -= pickupCap;
+//                     pickupCap = 0;
+//                     pStack.push(temp);
+//                 }
+//             }
+            
+//             while (deliverCap != 0 && !dStack.isEmpty()) {
+//                 int[] temp = dStack.pop();
+//                 if (temp[1] <= deliverCap) {
+//                     deliverCap -= temp[1];
+//                 } else {
+//                     temp[1] -= deliverCap;
+//                     deliverCap = 0;
+//                     dStack.push(temp);
+//                 }
+//             }
+            
+//         }
+//         return answer;
+//     }
+// }
