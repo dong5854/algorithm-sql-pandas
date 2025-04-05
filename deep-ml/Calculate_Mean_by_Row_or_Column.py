68824356946dc03c466f910251e31335598d3dc1
dong young <dong5854@gmail.com>
2025-04-06 00:10:49 +0900

    deep-ml(python): Calculate Mean by Row or Column

--- deep-ml/Calculate_Mean_by_Row_or_Column.py
+++ deep-ml/Calculate_Mean_by_Row_or_Column.py
@@ -4,4 +4,4 @@ def calculate_matrix_mean(matrix: list[list[float]], mode: str) -> list[float]:
         means = [sum(row, 0.0) / len(row) for row in matrix]
     elif mode == "column":
         means = [sum(col, 0.0) / len(col) for col in zip(*matrix)]
-	return means
+    return means
